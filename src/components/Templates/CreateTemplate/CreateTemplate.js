b1442afc0eb081fba5339bcae3307f02790b0dce
LauraOrozco26 <141424974+LauraOrozco26@users.noreply.github.com>
2024-02-19 08:38:06 -0500

    cambios

--- src/components/Templates/CreateTemplate/CreateTemplate.js
+++ src/components/Templates/CreateTemplate/CreateTemplate.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreateTemplate.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
-
+import { faQuestionCircle, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
 
 const CreateTemplate = () => {
+  const [campaignId, setCampaignId] = useState('');
+  const [templateTitle, setTemplateTitle] = useState('');
+  const [templateLanguage, setTemplateLanguage] = useState('Seleccione una opción');
+  const [templateType, setTemplateType] = useState('Seleccione una opción');
+  const [templateContent, setTemplateContent] = useState('');
+  
   return (
     <div className="ContainerPrincipal">
       <div className="ContainerSecundario">
@@ -14,51 +19,82 @@ const CreateTemplate = () => {
         <div className="contentt">
           <div className="ContenedorInput">
             <label htmlFor="campaign-id" className="Etiqueta" >ID de la Campaña:</label>
-            <input type="text" id="campaign-id" name="campaign-id" className="Input" placeholder='Ingrese el Id del template' />
+            <input 
+              type="text" 
+              id="campaign-id" 
+              name="campaign-id" 
+              className="Input" 
+              placeholder='Ingrese el Id del template' 
+              value={campaignId} 
+              onChange={(e) => setCampaignId(e.target.value)} 
+            />
             <div className="IconoInterrogacion" title="Este campo es para ingresar el ID de la Campaña">
-            <FontAwesomeIcon icon={faQuestionCircle} className="IconoInterrogacion" title="Este campo es para ingresar el ID de la Campaña" />
-
+              <FontAwesomeIcon icon={faQuestionCircle} className="IconoInterrogacion" title="Este campo es para ingresar el ID de la Campaña" />
             </div>
           </div>
 
           <div className="ContenedorInput">
             <label htmlFor="template-title" className="Etiqueta">Título del Template:</label>
-            <input type="text" id="template-title" name="template-title" className="Input" placeholder='Ingrese título del template'/>
+            <input 
+              type="text" 
+              id="template-title" 
+              name="template-title" 
+              className="Input" 
+              placeholder='Ingrese título del template'
+              value={templateTitle} 
+              onChange={(e) => setTemplateTitle(e.target.value)} 
+            />
             <div className="IconoInterrogacion" title="Este campo es para ingresar el título del Template">
-            <FontAwesomeIcon icon={faQuestionCircle} className="IconoInterrogacion" title="Este campo es para ingresar el ID de la Campaña" />
-
+              <FontAwesomeIcon icon={faQuestionCircle} className="IconoInterrogacion" title="Este campo es para ingresar el ID de la Campaña" />
             </div>
           </div>
 
           <div className="ContenedorInput">
             <label htmlFor="template-language" className="Etiqueta">Idioma:</label>
-            <select id="template-language" name="template-language" className="Select" >
-              <option value="image">Seleccione una opción</option>
+            <select 
+              id="template-language" 
+              name="template-language" 
+              className="Select" 
+              value={templateLanguage} 
+              onChange={(e) => setTemplateLanguage(e.target.value)}
+            >
+              <option value="Seleccione una opción">Seleccione una opción</option>
               <option value="spanish">Español</option>
               <option value="english">Inglés</option>
             </select>
             <div className="IconoInterrogacion" title="Este campo es para seleccionar el idioma del Template">
-            <FontAwesomeIcon icon={faQuestionCircle} className="IconoInterrogacion" title="Este campo es para ingresar el ID de la Campaña" />
-
+              <FontAwesomeIcon icon={faQuestionCircle} className="IconoInterrogacion" title="Este campo es para ingresar el ID de la Campaña" />
             </div>
           </div>
 
           <div className="ContenedorInput">
             <label htmlFor="template-type" className="Etiqueta">Tipo de Template:</label>
-            <select id="template-type" name="template-type" className="Select">
-              <option value="image">Seleccione una opción</option>
+            <select 
+              id="template-type" 
+              name="template-type" 
+              className="Select"
+              value={templateType} 
+              onChange={(e) => setTemplateType(e.target.value)}
+            >
+              <option value="Seleccione una opción">Seleccione una opción</option>
               <option value="image">Imagen</option>
               <option value="text">Texto</option>
             </select>
             <div className="IconoInterrogacion" title="Este campo es para seleccionar el tipo de Template">
-            <FontAwesomeIcon icon={faQuestionCircle} className="IconoInterrogacion" title="Este campo es para ingresar el ID de la Campaña" />
-
+              <FontAwesomeIcon icon={faQuestionCircle} className="IconoInterrogacion" title="Este campo es para ingresar el ID de la Campaña" />
             </div>
           </div>
 
           <div className="ContenedorInput">
             <label htmlFor="template-content" className="Etiqueta">Contenido:</label>
-            <input type="text" id="template-content" name="template-content" className="InputAncho" />
+            <input 
+              type="text" 
+              id="template-content" 
+              name="template-content" 
+              className="InputAncho" 
+              value={templateContent} 
+              onChange={(e) => setTemplateContent(e.target.value)}
+            />
             <div className="IconoInterrogacion" title="Este campo es para ingresar el contenido del Template">
             </div>
           </div>
@@ -66,7 +102,16 @@ const CreateTemplate = () => {
           <div className="VisualizacionCelular">
             <h2>Visualización en Celular</h2>
             <div className="Celular">
-
+              <FontAwesomeIcon icon={faMobileAlt} className="IconoCelular" />
+              <div className="PantallaCelular">
+                <div className="ContenidoCelular">
+                  <p><strong>ID de la Campaña:</strong> {campaignId}</p>
+                  <p><strong>Título del Template:</strong> {templateTitle}</p>
+                  <p><strong>Idioma:</strong> {templateLanguage}</p>
+                  <p><strong>Tipo de Template:</strong> {templateType}</p>
+                  <p><strong>Contenido:</strong> {templateContent}</p>
+                </div>
+              </div>
             </div>
           </div>
           <button type="submit" className="Boton">Guardar</button>
--- src/components/Templates/Templates.js
+++ src/components/Templates/Templates.js
@@ -3,22 +3,51 @@ import { useNavigate } from 'react-router-dom';
 import './Templates.scss';
 import ModalEliminar from './ModalEliminar/modalEliminar';
 import ModalEditar from './ModalEditar/modalEditar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes, faSpinner } from '@fortawesome/free-solid-svg-icons'; // Importa los íconos de FontAwesome
 
 function Templates() {
   const [selectedRows, setSelectedRows] = useState([]);
   const [showModalEliminar, setShowModalEliminar] = useState(false);
   const [showModalEditar, setShowModalEditar] = useState(false);
-
   const navigate = useNavigate();
 
-  const handleRowClick = (index) => {
-    let newSelectedRows = [...selectedRows];
-    if (newSelectedRows.includes(index)) {
-      newSelectedRows = newSelectedRows.filter((row) => row !== index);
-    } else {
-      newSelectedRows.push(index);
+  // Datos de ejemplo quemados
+  const templateData = [
+    {
+      id: 1,
+      title: 'Plantilla de ejemplo 1',
+      type: 'Tipo 1',
+      message: 'Mensaje de ejemplo 1',
+      category: 'Categoría A',
+      status: 'Activa'
+    },
+    {
+      id: 2,
+      title: 'Plantilla de ejemplo 2',
+      type: 'Tipo 2',
+      message: 'Mensaje de ejemplo 2',
+      category: 'Categoría B',
+      status: 'Rechazada'
+    },
+    {
+      id: 3,
+      title: 'Plantilla de ejemplo 3',
+      type: 'Tipo 3',
+      message: 'Mensaje de ejemplo 3',
+      category: 'Categoría C',
+      status: 'Pendiente'
     }
-    setSelectedRows(newSelectedRows);
+  ];
+
+  const handleRowClick = (id) => {
+    setSelectedRows(prevSelectedRows => {
+      if (prevSelectedRows.includes(id)) {
+        return prevSelectedRows.filter(row => row !== id);
+      } else {
+        return [...prevSelectedRows, id];
+      }
+    });
   };
 
   const handleDeleteButtonClick = () => {
@@ -65,67 +94,42 @@ function Templates() {
             <input type="text" placeholder="Ingrese el título" className='inputbuscar' />
             <button className='buttonFiltrar'>Filtrar y Buscar</button>
           </div>
-          <div className='containerbtn'>
-            <button className='btneliminar' onClick={handleDeleteButtonClick}>Eliminar</button>
-            <button className='btneditar' onClick={handleEditButtonClick}>Editar</button>
+          <div className="table-container">
+            {templateData.map(template => (
+              <div key={template.id} className={`template-item2 ${selectedRows.includes(template.id) ? 'selected' : ''}`} onClick={() => handleRowClick(template.id)}>
+                <div className="template-info-box"> {/* Cuadro con borde redondeado y color de fondo gris */}
+                  <input type="checkbox" checked={selectedRows.includes(template.id)} onChange={() => {}} />
+                  <div className="template-info">
+                    <h3 className="template-title">{template.title}</h3>
+                    <p className="template-detail"><strong>Tipo:</strong> {template.type}</p>
+                    <p className="template-detail"><strong>Mensaje:</strong> {template.message}</p>
+                    <p className="template-detail"><strong>Categoría:</strong> {template.category}</p>
+                    <p className="template-detail"><strong>Estado:</strong> {template.status} {' '}
+                      {template.status === 'Activa' && <FontAwesomeIcon icon={faCheck} color="green" />} 
+                      {template.status === 'Rechazada' && <FontAwesomeIcon icon={faTimes} color="red" />}
+                      {template.status === 'Pendiente' && <FontAwesomeIcon icon={faSpinner} className="spinning" />}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-          <table className="table">
-            <thead>
-              <tr>
-                <th className='th'><input type="checkbox" /></th>
-                <th className='th'>Title</th>
-                <th className='th'>Type</th>
-                <th className='th'>Message</th>
-                <th className='th'>Category</th>
-                <th className='th'>Status</th>
-                <th className='th'>Preview</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr onClick={() => handleRowClick(1)} className={selectedRows.includes(1) ? 'selected' : ''}>
-                <td className='td'><input type="checkbox" checked={selectedRows.includes(1)} onChange={() => { }} /></td>
-                <td className='td'>Ejemplo de título</td>
-                <td className='td'>Ejemplo de tipo</td>
-                <td className='td'>Ejemplo de mensaje</td>
-                <td className='td'>Ejemplo de categoría</td>
-                <td className='td'>
-                  <div className="status-circle">
-                    <span className="check-mark">&#10003;</span>
-                  </div>
-                </td>
-                <td className='td'>
-                  <span className="preview-icon">&#128065;</span>
-                </td>
-              </tr>
-              <tr onClick={() => handleRowClick(0)} className={selectedRows.includes(0) ? 'selected' : ''}>
-                <td className='td'><input type="checkbox" checked={selectedRows.includes(0)} onChange={() => { }} /></td>
-                <td className='td'>Ejemplo de título</td>
-                <td className='td'> Ejemplo de tipo</td>
-                <td className='td'>Ejemplo de mensaje</td>
-                <td className='td'>Ejemplo de categoría</td>
-                <td className='td'>
-                  <div className="status-circle-equis">
-                    <span className="check-Equis">&#10007;</span>
-                  </div>
-                </td>
-                <td className='td'>
-                  <span className="preview-icon">&#128065;</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
         </div>
-        <ModalEliminar
-          showModal={showModalEliminar}
-          handleCancelDelete={handleCancelEliminar}
-          handleConfirmDelete={handleConfirmEliminar}
-        />
-        <ModalEditar
-          showModal={showModalEditar}
-          handleCancelEdit={handleCancelEdit}
-          handleConfirmEdit={handleConfirmEdit}
-        />
+        <div className="action-buttons">
+          <button className='btneliminar' onClick={handleDeleteButtonClick}>Eliminar</button>
+          <button className='btneditar' onClick={handleEditButtonClick}>Editar</button>
+        </div>
       </div>
+      <ModalEliminar
+        showModal={showModalEliminar}
+        handleCancelDelete={handleCancelEliminar}
+        handleConfirmDelete={handleConfirmEliminar}
+      />
+      <ModalEditar
+        showModal={showModalEditar}
+        handleCancelEdit={handleCancelEdit}
+        handleConfirmEdit={handleConfirmEdit}
+      />
     </div>
   );
 }
